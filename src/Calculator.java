31d69d8364fa929c88674e70bd39b1f2defd8de7
Mishanya.Melnikov <gallter@ya.ru>
2022-05-17 21:56:16 +0300

    second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,18 +7,15 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (x == 0 || y == 0) {
+            System.out.println("Деление на ноль");
+        }
+        return x/y;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
-    UnaryOperator<Integer> abs = x -> {  // в изначальном методе не отслеживалось деление на 0
-        if (x == 0) {
-            System.out.println("Деление на ноль");
-
-        } else if (x < 0) {
-            x = x * -1;
-        }
-        return x;
-    };
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
     Predicate<Integer> isPositive = x -> x > 0;
 
